87c99d880d19a890976dd4bf431761f4a51c5046
Luan Oliveira <olivrluan@gmail.com>
2025-07-11 01:10:31 -0300

    feat(repository): adiciona interface FundoRepository estendendo JpaRepository

---
+++ src/main/java/com/luan/funds/repository/FundoRepository.java
@@ -0,0 +1,9 @@
+package com.luan.funds.repository;
+
+
+import com.luan.funds.model.Fundo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FundoRepository extends JpaRepository<Fundo,Long> {
+
+}
