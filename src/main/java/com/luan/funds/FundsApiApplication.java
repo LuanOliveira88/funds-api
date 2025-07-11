cb18f08ed4facc78d8dbb058c7575801bbb1583b
Luan Oliveira <olivrluan@gmail.com>
2025-07-11 08:10:11 -0300

    feat(core): adiciona classe principal FundsApiApplication com configuração base do Spring Boot

---
+++ src/main/java/com/luan/funds/FundsApiApplication.java
@@ -0,0 +1,13 @@
+package com.luan.funds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundsApiApplication.class, args);
+	}
+
+}
